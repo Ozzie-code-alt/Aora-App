2318d105e67f61d9a136b617b192be345ef441ec
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2024-05-24 15:25:06 +0800

    feature: tabs and tab styles with dynamic routes

--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
-import {Link} from 'expo-router'
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+import { Link } from "expo-router";
+
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl">Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-      <Link href="/profile" className='text-blue-500'>to Profile</Link>
-    </View>
+    <>
+      <View className="flex-1 items-center justify-center bg-white">
+        <Text className="text-3xl font-pbold">Hello World</Text>
+        <StatusBar style="auto" />
+        <Link href="/home" className="text-blue-500">
+          Go to Home
+        </Link>
+      </View>
+    </>
   );
 }
-
